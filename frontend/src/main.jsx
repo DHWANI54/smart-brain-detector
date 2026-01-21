4c5d93005da971935331ded29f5cbc49d9e4dfd9
DHWANI54 <dhwaniagrawal214@gmail.com>
2026-01-21 10:25:54 +0530

    Reapply "Fixed Issues"
    This reverts commit 166707b6f300b11880109e82e08d0b94081cd24f.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom'; // ✅ this line is crucial
 import './index.css';
+import 'tachyons';
 import App from './App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
